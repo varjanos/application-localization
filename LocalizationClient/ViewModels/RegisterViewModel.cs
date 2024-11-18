215fad24ee42538fedb10d9a1351ba4cf50490f4
atkalcecpeti <55134958+atkalcecpeti@users.noreply.github.com>
2024-11-18 20:09:49 +0100

    Add files via upload

---
+++ LocalizationClient/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using LocalizationClient.Model;
+using LocalizationClient.Services;
+
+namespace LocalizationClient.ViewModels
+{
+    public class RegisterViewModel
+    {
+        public UserService userService = UserService.Instance();
+        public User userToRegister = new User();
+        public bool RegisterService(User user)
+        {
+            if (userService.Register(user))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
