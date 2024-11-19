357dbcd72234453a5835c3bcc2f6d53bc5451d47
atkalcecpeti <tkalcec.peter@edu.bme.hu>
2024-11-19 16:30:00 +0100

    localization service added in program.cs english resource added

--- LocalizationClient/Program.cs
+++ LocalizationClient/Program.cs
@@ -9,8 +9,16 @@ builder.Services.AddRazorComponents()
 builder.Services.AddScoped<LoginViewModel>();
 builder.Services.AddScoped<RegisterViewModel>();
 builder.Services.AddScoped<LocalDateViewModel>();
+builder.Services.AddLocalization();
 
 var app = builder.Build();
+
+string[] appCultures = ["en-UK", "h-Hun"];
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(appCultures[0])
+    .AddSupportedCultures(appCultures)
+    .AddSupportedUICultures(appCultures);
+app.UseRequestLocalization(localizationOptions);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
