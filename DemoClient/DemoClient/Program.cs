9462d690f09d4942d1f0f1086f79b917ca3aec7c
János Varga <var.janos.hu@gmail.com>
2024-11-22 17:07:30 +0100

    Added demo client

---
+++ DemoClient/DemoClient/Program.cs
@@ -0,0 +1,13 @@
+using DemoClient;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddLocalization(opts => { opts.ResourcesPath = "Resources"; });
+
+await builder.Build().RunAsync();
